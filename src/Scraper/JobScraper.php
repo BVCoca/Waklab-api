ea4a319f1f6e862b80fcd039027167be0d288bbf
Alexis Migniau <migniau.alexis@gmail.com>
2023-10-15 13:54:15 +0200

    feat(job) : Création du FakeScraper de Job

---
+++ src/Scraper/JobScraper.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace App\Scraper;
+
+use App\Entity\Job;
+
+class JobScraper extends Scraper {
+
+    public function getUrl(): string {
+        return '';
+    }
+
+    public function getEntities() : array
+    {
+        return [
+            Job::class
+        ];
+    }
+
+    public function getName() : string
+    {
+        return 'Jon';
+    }
+
+    public function scrap(array &$scraped_data): array {
+        $datas = [
+            [
+                'Forestier',
+                '',
+                'collecte'
+            ],
+            [
+                'Herboriste',
+                '',
+                'collecte'
+            ],
+            [
+                'Mineur',
+                '',
+                'collecte'
+            ],
+            [
+                'Paysan',
+                '',
+                'collecte'
+            ],
+            [
+                'Pêcheur',
+                '',
+                'collecte'
+            ],
+            [
+                'Trappeur',
+                '',
+                'collecte'
+            ],
+            [
+                'Armurier',
+                '',
+                'artisanat'
+            ],
+            [
+                'Bijoutier',
+                '',
+                'artisanat'
+            ],
+            [
+                'Boulanger',
+                '',
+                'artisanat'
+            ],
+            [
+                'Cuisinier',
+                '',
+                'artisanat'
+            ],
+            [
+                'Ébéniste',
+                '',
+                'artisanat'
+            ],
+            [
+                'Maitre d\'Armes',
+                '',
+                'artisanat'
+            ],
+            [
+                'Maroquinier',
+                '',
+                'artisanat'
+            ],
+            [
+                'Tailleur',
+                '',
+                'artisanat'
+            ],
+        ];
+
+        return array_map(function($data) use(&$scraped_data) {
+            $obj = new Job();
+            $obj->setName($data[0]);
+            $obj->setIcon($data[1]);
+            $obj->setType($data[2]);
+
+            $scraped_data['job'][$data[0]] = $obj;
+
+            $this->entityManager->persist($obj);
+            $this->entityManager->flush();
+
+            return $obj;
+        }, $datas);
+    }
+
+    /**
+     * Pas utilisée
+     */
+    public function getEntityData(string $slug, array &$scraped_data = []) {
+       
+    }
+}
