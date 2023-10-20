53cd0682356d2774f5c59777527f1bafcc8654ac
Alexis Migniau <migniau.alexis@gmail.com>
2023-10-20 21:44:48 +0200

    feat(search) : Fonctionnalité de recherche multi index

---
+++ src/Search/MultiIndex.php
@@ -0,0 +1,99 @@
+<?php
+
+/*
+ * Created by Exploit.cz <insekticid AT exploit.cz>
+ */
+
+namespace App\Search;
+
+use Elastica\Exception\InvalidException;
+use Elastica\Index;
+use Elastica\ResultSet\BuilderInterface;
+use Elastica\Search;
+
+class MultiIndex extends Index
+{
+    /**
+     * Array of indices.
+     *
+     * @var array
+     */
+    protected $_indices = [];
+
+    /**
+     * Adds a index to the list.
+     *
+     * @param \Elastica\Index|string $index Index object or string
+     *
+     * @throws \Elastica\Exception\InvalidException
+     *
+     * @return $this
+     */
+    public function addIndex($index)
+    {
+        if ($index instanceof Index) {
+            $index = $index->getName();
+        }
+
+        if (!is_scalar($index)) {
+            throw new InvalidException('Invalid param type');
+        }
+
+        $this->_indices[] = (string) $index;
+
+        return $this;
+    }
+
+    /**
+     * Add array of indices at once.
+     *
+     * @param array $indices
+     *
+     * @return $this
+     */
+    public function addIndices(array $indices = [])
+    {
+        foreach ($indices as $index) {
+            $this->addIndex($index);
+        }
+
+        return $this;
+    }
+
+    /**
+     * Set array of indices
+     * @param array $indices
+     *
+     * @return $this
+     */
+    public function setIndices(array $indices = [])
+    {
+        $this->_indices = $indices;
+    }
+
+    /**
+     * Return array of indices.
+     *
+     * @return array List of index names
+     */
+    public function getIndices()
+    {
+        return $this->_indices;
+    }
+
+    /**
+     * @param string|array|\Elastica\Query $query
+     * @param int|array                    $options
+     * @param BuilderInterface             $builder
+     *
+     * @return Search
+     */
+    public function createSearch($query = '', $options = null, ?BuilderInterface $builder = null): Search
+    {
+        $search = new Search($this->getClient(), $builder);
+        $search->addIndices($this->getIndices());
+        $search->setOptionsAndQuery($options, $query);
+
+        return $search;
+    }
+}
---
+++ src/Search/PagerFantaToPaginator.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Search;
+
+use ApiPlatform\State\Pagination\PaginatorInterface;
+use Pagerfanta\Pagerfanta;
+
+class PagerFantaToPaginator implements PaginatorInterface, \IteratorAggregate {
+   
+    private Pagerfanta $pagerfanta;
+
+    public function __construct(Pagerfanta $pagerfanta)
+    {
+        $this->pagerfanta = $pagerfanta;
+    }
+
+    public function count(): int
+    {
+        return $this->pagerfanta->getNbResults();
+    }
+
+    public function getLastPage(): float {
+        return $this->pagerfanta->getNbPages();
+    }
+
+   
+    public function getTotalItems(): float {
+        return $this->pagerfanta->getNbResults();
+    }
+
+    public function getCurrentPage(): float {
+        return $this->pagerfanta->getCurrentPage();
+    }
+
+    public function getItemsPerPage(): float {
+        return $this->pagerfanta->getMaxPerPage();
+    }
+
+    /**
+     * @return \Traversable<array-key, T>
+     */
+    public function getIterator(): \Traversable
+    {
+        return $this->pagerfanta->getIterator();
+    }
+}
